2559d4c9eba2d2ff0d3b3a8935ed47c55fb3a0f0
adamem02 <adamem619@gmail.com>
2024-01-14 00:23:31 -0800

    added time blocks for hours

--- Develop/script.js
+++ Develop/script.js
@@ -5,5 +5,29 @@ $(document).ready(function () {
 
   //Generate time blocks
   var container = $("#time-block-container");
+  var businessHours = 9;
+  var currentTime = dayjs().hour();
+  var hoursToShow = 14;
+var endHour = businessHours + hoursToShow;
+
+// Generate tine blocks for the desired number of hours
+for (let i=businessHours; i < endHour; i++) {
+  var timeBlock = $("<div>")
+  .addClass("row time-block")
+  .attr("id", "hour-" + i)
+  .appendTo(container);
+
+  var hourCol = $("<div>")
+  .addClass("col-2 col-md-1 hour text-center py-3")
+  .text(dayjs().hour(i).format("hA"))
+  .appendTo(timeBlock);
+
+  var descriptionCol = $("<textarea>")
+  .addClass("col-8 col-md-10 description")
+  .attr("rows", "3")
+  .val(localStorage.getItem("hour-" + i))
+  .appendTo(timeBlock);
+
   
+}
 });
